80acf53dc9da4c6272c4572baa9512edf895452c
abdourazakaden <abdourazakaden1234@gmail.com>
2026-03-04 13:59:10 +0530

    creation de application streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,14 @@
 import streamlit as st
-st.title ("hello Abdourazak Aden")  
+
+st.title("Bienvenue Abdirahman 👋")
+
+st.header("Je découvre Streamlit")
+
+nom = st.text_input("Entrez votre nom")
+
+if st.button("Valider"):
+    st.success(f"Bonjour {nom} 🚀")
+
+age = st.slider("Choisissez votre âge", 0, 100, 20)
+
+st.write("Votre âge est :", age)
